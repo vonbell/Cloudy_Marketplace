375edcda2afa5174c1316ba7d8544484b1a08666
vonbell <divonvbell@gmail.com>
2022-07-19 11:46:32 -0400

    added ETH prices

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -30,8 +30,26 @@ const NFTCard = ({ data }) => {
 
                 <CircleButton imgUrl={assets.heart} right={10} top={10} />
             </View>
-
             <SubInfo />
+
+            <View style={{ width: '100%', padding: SIZES.font }}>
+                <NFTTitle 
+                    title={data.name}
+                    subTitle={data.creator}
+                    titleSize={SIZES.large}
+                    subTitleSize={SIZES.small}
+                />
+            </View>
+
+            <View style={{
+                marginTop: SIZES.font,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+                }}>
+                <ETHPrice price={data.price} />
+                <RectButton />
+            </View>
         </View>
     )
 }
