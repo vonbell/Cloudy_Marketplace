d39dee1c5feca8349f84ee19d00860379945692f
vonbell <divonvbell@gmail.com>
2022-07-20 19:30:15 -0400

    home header search bar

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,7 +1,7 @@
 import { View, Text, Image, TextInput } from 'react-native';
 import { COLORS, FONTS, SIZES, assets } from '../constants';
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
     return (
         <View
             style={{
@@ -32,6 +32,39 @@ const HomeHeader = () => {
                         source={assets.badge}
                         resizeMode='contain'
                         style={{ position: 'absolute', width: 15, height: 15, bottom: 0, right: 0 }}
+                    />
+                </View>
+            </View>
+
+            <View style={{ marginVertical: SIZES.font }}>
+                <Text style={{ fontSize: SIZES.small, color: COLORS.white }}>
+                    Hello, Tay 👋
+                </Text>
+                <Text style={{ fontSize: SIZES.large, color: COLORS.white, marginTop: SIZES.base / 2 }}>
+                    Let's find a masterpiece
+                </Text>
+            </View>
+
+            <View style={{ marginTop: SIZES.font }}>
+                <View 
+                    style={{
+                        width: '100%',
+                        borderRadius: SIZES.font,
+                        backgroundColor: COLORS.gray,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingHorizontal: SIZES.font,
+                        paddingVertical: SIZES.small - 2
+                    }}>
+                    <Image
+                        source={assets.search}
+                        resizeMode='contain'
+                        style={{ width: 20, height: 20, marginRight: SIZES.base }}
+                    />
+                    <TextInput
+                        placeholder='Search NFTs'
+                        style={{ flex: 1 }}
+                        onChangeText={onSearch}
                     />
                 </View>
             </View>
