bf44b0e60a57067fbb818b8a0d9cba661a94c0f9
vonbell <divonvbell@gmail.com>
2022-08-01 19:30:16 -0400

    detail desc data displayed w onpress

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -1,10 +1,56 @@
+import { useState } from 'react';
 import { View, Text } from 'react-native';
+import { ETHPrice, NFTTitle } from './SubInfo';
+import { COLORS, SIZES, FONTS } from '../constants';
 
-const DetailsDesc = () => {
+const DetailsDesc = ({ data }) => {
+    const [text, setText] = useState(data.description.slice(0, 100));
+    const [readMore, setReadMore] = useState(false);
+
     return (
-        <View>
-            <Text>DetailsDesc</Text>
-        </View>
+        <>
+            <View
+                style={{
+                    width: '100%',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}>
+                <NFTTitle
+                    title={data.name}
+                    subTitle={data.creator}
+                    titleSize={SIZES.extraLarge}
+                    subTitleSize={SIZES.font}
+                />
+
+                <ETHPrice price={data.price} />
+            </View>
+
+            <View style={{ marginVertical: SIZES.extraLarge * 1.5 }}>
+                <Text style={{ fontSize: SIZES.font, color: COLORS.primary }}>
+                    Description
+                </Text>
+                <View style={{ marginTop: SIZES.base }}>
+                    <Text style={{ fontSize: SIZES.small, color: COLORS.secondary, lineHeight: SIZES.large }}>
+                        {text}
+                        {!readMore && '...'}
+                        <Text 
+                            style={{ fontSize: SIZES.small, color: COLORS.primary }}
+                            onPress={() =>{
+                                if(!readMore) {
+                                    setText(data.description);
+                                    setReadMore(true);
+                                } else {
+                                    setText(data.description.slice(0, 100));
+                                    setReadMore(false);
+                                }
+                            }}>
+                            {readMore ? ' Show Less' : ' Read More'}
+                        </Text>
+                    </Text>
+                </View>
+            </View>
+        </>
     )
 }
 
